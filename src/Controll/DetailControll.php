1078c3c6d39946d18232692ceb64a4d89b92e86c
webdevned <griganed.web@gmail.com>
2022-06-15 14:45:01 +0300

    Twig implemented

--- src/Controll/DetailControll.php
+++ src/Controll/DetailControll.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace App\Controll;
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 class DetailControll {
     public function __construct() {
-        echo 'detail omg';
+        $loader = new FilesystemLoader(dirname(__DIR__, 1) . '/templates');
+        $twig = new Environment($loader);
+
+        echo $twig->render('base.html.twig', [
+            'title' => 'Controller',
+            'controller' => 'DetailControll'
+        ]);
     }
 }
